d9f9678ba818e4b66bbf6fd306a5a79f025d804b
Spaceplex <spaecplex@gmail.com>
2025-04-11 15:22:57 -0400

    Enemy stuff

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ThirdPersonController : MonoBehaviour
+public class EnemyController : MonoBehaviour
 {
+
+    public Image healthBar;
+    private float health = 20.0f;  
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +17,11 @@ public class ThirdPersonController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void TakeDamage()
+    {
+      health--;
+      healthBar.fillAmount = health/20.0f;
     }
 }
