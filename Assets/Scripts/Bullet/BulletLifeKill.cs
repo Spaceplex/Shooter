08d4ab406de7c353cd48f0c9c7b6ac14f98c66be
Spaceplex <spaecplex@gmail.com>
2025-04-11 13:46:13 -0400

    Shoots in the right direction

--- Assets/Scripts/Bullet/BulletLifeKill.cs
+++ Assets/Scripts/Bullet/BulletLifeKill.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 public class BulletLifeKill : MonoBehaviour
 {
 
-    public Transform orientation;
+    private Rigidbody bulletRB;
 
-    float speed = 100.0f;
+    private float speed = 100.0f;
+    public Vector3 forceEnd;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void Start()
     {
-        orientation = GameObject.FindGameObjectWithTag("Gun").transform;
+        bulletRB = GetComponent<Rigidbody>();
+        bulletRB.linearVelocity = transform.forward * speed;
         StartCoroutine(BKill());
-        transform.rotation = orientation.rotation;
     }
 
     void FixedUpdate()
     {
-        transform.Translate(transform.forward * speed * Time.deltaTime);
+        /*transform.Translate(transform.forward * speed * Time.deltaTime);*/
+        bulletRB.AddForce(forceEnd, ForceMode.Force);
     }
 
     IEnumerator BKill(){
--- Assets/Scripts/Player/PlayerGun.cs
+++ Assets/Scripts/Player/PlayerGun.cs
@@ -4,10 +4,13 @@ public class PlayerGun : MonoBehaviour
 {
 
     public ThirdPersonCam camScript;
-    
     public Transform gunBarrelTransform;
+
     [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
     public GameObject bullet;
+    private Vector3 mouseWorldPosition = Vector3.zero;
+
+    public Transform bulletDirectionBackup;
 
     bool combat;
 
@@ -23,14 +26,23 @@ public class PlayerGun : MonoBehaviour
     }
 
     private void gunShoot(){
-        //Vector2 screenCenterPoint = new Vector2(Screen.width/2f, Screen.height/2f);
-        //Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
-        //if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask)){
-        //bulletDirection.transform.position = raycastHit.point;
-        Instantiate(bullet, gunBarrelTransform.position, Camera.main.transform.rotation);
-        //} else {
-        //    return;
-        //}
+        Vector2 screenCenterPoint = new Vector2(Screen.width/2f, Screen.height/2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        Vector3 bulletDirection = Vector3.zero;
 
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask)){
+          bulletDirection = raycastHit.point - transform.position;
+          Debug.Log(bulletDirection);
+        } else {
+          bulletDirection = bulletDirectionBackup.position - transform.position;
+        }
+
+        Vector3 aimDir = (mouseWorldPosition - gunBarrelTransform.position).normalized;
+        GameObject go = Instantiate(
+            bullet, 
+            gunBarrelTransform.position, 
+            Quaternion.LookRotation(bulletDirection, Vector3.up)
+        );
+        go.GetComponent<BulletLifeKill>().forceEnd = bulletDirection;
     }
 }
